2ec51c002d59dacbcc5d6d64b839ef657541a1ca
AD <ad-it-works@outlook.com>
2025-04-25 22:33:09 +0800

    final01

--- leap_years.cpp
+++ leap_years.cpp
@@ -1,3 +1,20 @@
+/*
+    leap_years
+    Display the leap years of civil/comon (Gregorian Calendar) 
+    from year 1800 upto 2400.
+
+    For submission to https://code.golf/leap-years#cpp
+
+    Created by amd
+
+*/
+
+
+// ------------------------------
+// ******************************
+// Preprocessor Declaration
+// ******************************
+// ------------------------------
 #define YEAR_MIN 1800
 #define YEAR_MAX 2400
 
@@ -5,31 +22,62 @@
 
 using namespace std;
 
+
+
+// ------------------------------
+// ******************************
+// Function
+// leapyear_inquire
+// ******************************
+// ------------------------------
 bool leapyear_inquire(int year)
 {
+    // ----------------------------
+    // Variable declaration
     bool res;
-
     int x;
 
+    // ---------------------------
+    // Actual Function Run
+
+    // getting the year from function arg
     x=year;
 
+    // Test the variable for leap year
     if( (x%4 == 0 && x%100 !=0) || (x % 400 == 0))
     {
+        // situation if leap year
         res=true;
     }else
     {
+        // situation if ordinary year
         res=false;
     };
 
+    // return the result
     return res;
 };
 
+
+
+// ------------------------------
+// ******************************
+// Program Entry Point
+// ******************************
+// ------------------------------
 int main(int argc, char* argv[])
 {
+    // ----------------------------
+    // Variable declaration
     int c;
 
+    // ---------------------------
+    // Actual program Run
+
+    // scanning the ARGs for leap year
     for(c=YEAR_MIN; c<=YEAR_MAX; c++)
     {
+        // If Leap Year print the it
         if( (leapyear_inquire(c) ) == true )
         {
             cout<<c<<endl;
@@ -37,6 +85,7 @@ int main(int argc, char* argv[])
 
     };
 
+    // Program End and return the result
     return 0;
 
 };
